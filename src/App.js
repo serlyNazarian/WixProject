8552c0499a07dc5bfa9e0579ad5c2dcba12e5caa
serlyNazarian <serlynazarian@gmail.com>
2022-09-15 18:47:11 +0400

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import MainPage from './MainPage/MainPage';
-import MainPageSecond from './SecondPage/SecondPage';
-import MainPageThird from './ThirdPage/ThirdPage';
-import MainPageFourth from './FourthPage/FourthPage';
-import MainPageFifth from './FifthPage/FifthPage';
-import ContactForm from './MyForm/ContactForm';
+import MainPage from './components/MainPage/MainPage';
+import MainPageSecond from './components/SecondPage/SecondPage';
+import MainPageThird from './components/ThirdPage/ThirdPage';
+import MainPageFourth from './components/FourthPage/FourthPage';
+import MainPageFifth from './components/FifthPage/FifthPage';
+import ContactForm from './components/MyForm/ContactForm';
 
 function App() {
   return <div className="App">
