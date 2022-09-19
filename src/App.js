e2462287d9568ab290d955512c95b65dcf7de0a6
serlyNazarian <serlynazarian@gmail.com>
2022-09-19 14:16:56 +0400

    main

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import "./App.css";
-import MainPage from './components/MainPage/MainPage';
-import MainPageSecond from './components/SecondPage/SecondPage';
-import MainPageThird from './components/ThirdPage/ThirdPage';
-import MainPageFourth from './components/FourthPage/FourthPage';
-import MainPageFifth from './components/FifthPage/FifthPage';
-import ContactForm from './components/MyForm/ContactForm';
+import MainPage from "./components/MainPage/MainPage";
+import MainPageThird from "./components/ThirdPage/ThirdPage";
+import MainPageFourth from "./components/FourthPage/FourthPage";
+import MainPageFifth from "./components/FifthPage/FifthPage";
+import ContactForm from "./components/MyForm/ContactForm";
+import SecondPage from "./components/SecondPage/SecondPage";
 
 function App() {
-  return <div className="App">
-  <MainPage />
-  <MainPageSecond />
-  <MainPageThird />
-  <MainPageFourth />
-  <MainPageFifth />
-  <ContactForm />
-  </div>;
+  return (
+    <div className="App">
+      <MainPage />
+      <SecondPage />
+      <MainPageThird />
+      <MainPageFourth />
+      <MainPageFifth />
+      <ContactForm />
+    </div>
+  );
 }
 
 export default App;
--- src/components/SecondPage/SecondPage.js
+++ src/components/SecondPage/SecondPage.js
@@ -1,76 +1,40 @@
-import './SecondPage.css';
-import MyImage from '../img/2.png';
-import MySecondImage from '../img/3.png';
-import MyThirdImage from '../img/4.webp';
-import MyFourthImage from '../img/5.webp';
-import MyFifthImage from '../img/6.webp';
-import MySixthImage from '../img/7.webp';
-import MySeventhImage from '../img/8.webp';
-import MyEightImage from '../img/9.webp';
-import MyNinthImage from '../img/10.webp';
+import React, { useState } from 'react';
 
-const MainPageSecond = () => {
+// import MyImage from './imgs/2.png';
+import './SecondPage.css';
+
+const SecondPage = () => {
+
+  const [images] = useState([
+    {id: 1, name: 'MyFirstImage', path: require('./imgs/2.png')},
+    {id: 2, name: 'MySecondImage', path: require('./imgs/3.png')},
+    {id: 3, name: 'MyThirdImage', path: require('./imgs/4.webp')},
+    {id: 4, name: 'MyFourthImage', path: require('./imgs/5.webp')},
+    {id: 5, name: 'MyFifthImage', path: require('./imgs/6.webp')},
+    {id: 6, name: 'MySixthImage', path: require('./imgs/7.webp')},
+    {id: 7, name: 'MySeventhImage', path: require('./imgs/8.webp')},
+    {id: 8, name: 'MyEightImage', path: require('./imgs/9.webp')},
+    {id: 9, name: 'MyNinthImage', path: require('./imgs/10.webp')},
+  ])
+  // console.table(images);
   return (
   <div className="Page-2">
     <h1>Project</h1>
+     {/* <img src= {require('./imgs/2.png')} alt="cool-art11" /> */}
+     {/* <img src= {require(images[0].path)} alt="cool-art11" /> */}
+     {/* <img src= {require(images[0].path)} alt="cool-art11" /> */}
     <div className="items">
+      {images.map((img) =>(
       <div className="item">
-        <img src= {MyImage} alt="cool-art" />
+        <img key={img.id} src= {img.path} alt="cool-art" />
         <div className="hover-time">
-          <h3>Project 01</h3>
+          <h3>{img.id}</h3>
         </div>
       </div>
-      <div className="item">
-        <img src= {MySecondImage} alt="cool-art" />
-        <div className="hover-time">
-          <h3>Project 02</h3>
-        </div>
-      </div>
-      <div className="item">
-        <img src= {MyThirdImage} alt="cool-art" />
-        <div className="hover-time">
-          <h3>Project 03</h3>
-        </div>
-      </div>
-       <div className="item">
-        <img src= {MyFourthImage} alt="cool-art" />
-        <div className="hover-time">
-          <h3>Project 04</h3>
-        </div>
-      </div>
-      <div className="item">
-        <img src= {MyFifthImage} alt="cool-art" />
-        <div className="hover-time">
-          <h3>Project 05</h3>
-        </div>
-      </div>
-      <div className="item">
-        <img src= {MySixthImage} alt="cool-art" />
-        <div className="hover-time">
-          <h3>Project 06</h3>
-        </div>
-      </div>
-      <div className="item">
-        <img src= {MySeventhImage} alt="cool-art" />
-        <div className="hover-time">
-          <h3>Project 07</h3>
-        </div>
-      </div>
-      <div className="item">
-        <img src= {MyEightImage} alt="cool-art" />
-        <div className="hover-time">
-          <h3>Project 08</h3>
-        </div>
-      </div>
-      <div className="item">
-        <img src= {MyNinthImage} alt="cool-art" />
-        <div className="hover-time">
-          <h3>Project 09</h3>
-        </div>
-      </div>
+      ))}
     </div>
   </div>
   )
 }
-
-export default MainPageSecond;
+// console.count("allooo");
+export default SecondPage;
